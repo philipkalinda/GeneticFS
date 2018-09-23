79fa995701b4743685e80f99d08dad6c60a332f3
Philip Kalinda <philipkalinda@gmail.com>
2018-09-23 18:51:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
-
-from setuptools import setup
-from setuptools.extension import Extension
-
+from distutils.core import setup
 setup(
-    author="Philip Kalinda",
-    author_email="philipkalinda@gmail.com",
-    name='genetic_algorithm',
-    version='0.1',
-    url='https://github.com/philipkalinda/genetic_algorithm',
-    description='A Genetic Algorithm for feature selection in Data Science problems',
-    license='MIT',
-    install_requires=['matplotlib', 'numpy>=1.6.1', 'sklearn', 'numpy', 'time'],
-    ext_modules=[],
-    scripts=['genetic_algorithm.py'],
-    packages=['genetic_algorithm'],
-    package_data={'genetic_algorithm': ['stopwords', 'DroidSansMono.ttf']}
+  name = 'geneticfs',         # How you named your package folder (MyLib)
+  packages = ['geneticfs'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Genetic Algorithm to optimise feature selection in Machine Learning',   # Give a short description about your library
+  author = 'Philip Kalinda',                   # Type in your name
+  author_email = 'philipkalinda@gmail.com',      # Type in your E-Mail
+  url = 'https://philipkalinda.com',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/philipkalinda/GeneticFS/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['Genetic Algorithm', 'Algorithm', 'Machine Learning', 'Feature Selection', 'Optimisation'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'numpy',
+          'time',
+          'sklearn',
+          'matplotlib'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
 )
