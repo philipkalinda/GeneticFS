8f0f72088a2cfdb4ad4afd629d6bf10d46101034
Philip Kalinda <philipkalinda@gmail.com>
2018-09-23 20:36:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'geneticfs',
   packages = ['geneticfs'],
-  version = '0.01',
+  version = '0.02',
   license='MIT',
   description = 'Genetic Algorithm to optimise feature selection in Machine Learning',
   author = 'Philip Kalinda',
   author_email = 'philipkalinda@gmail.com',
   url = 'https://philipkalinda.com',
-  download_url = 'https://github.com/philipkalinda/GeneticFS/archive/v_001.tar.gz',
+  download_url = 'https://github.com/philipkalinda/GeneticFS/archive/v_002.tar.gz',
   keywords = ['Genetic Algorithm', 'Algorithm', 'Machine Learning', 'Feature Selection', 'Optimisation'],
   install_requires=[
           'numpy',
