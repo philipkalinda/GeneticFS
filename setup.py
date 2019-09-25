48ef51409abdd2901612bb335e30ddcc254e2585
Philip Kalinda <philipkalinda@gmail.com>
2019-09-25 11:56:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from distutils.core import setup
 setup(
-  name = 'StackerPy',
-  packages = ['stackerpy'],
+  name = 'GeneticFS',
+  packages = ['GeneticFS'],
   version = '0.01',
   license='MIT',
-  description = 'Machine Learning model stacking with blending capabilities (Compatible with Scikit-Learn)',
+  description = 'Genetic Algorithm for feature selection within Machine Learning',
   author = 'Philip Kalinda',
   author_email = 'philipkalinda@gmail.com',
   url = 'https://philipkalinda.com',
   download_url = 'https://github.com/philipkalinda/stackerpy/archive/v_001.tar.gz',
-  keywords = ['Stacking', 'Scikit-Learn', 'sklearn', 'Algorithm', 'Machine Learning', 'Blending', 'Optimisation'],
+  keywords = ['Genetic Algorithm', 'Scikit-Learn', 'sklearn', 'Algorithm', 'Machine Learning', 'Optimisation'],
   install_requires=[
           'numpy',
           'sklearn',
+          'matplotlib'
       ],
   classifiers=[
     'Development Status :: 1 - Alpha',
